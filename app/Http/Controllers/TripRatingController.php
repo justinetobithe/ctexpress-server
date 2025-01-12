e0000f9ef2ea92dc44110bf349427a62bcc9123d
Justine Tobithe Doloiras <justine.tobithe27@gmail.com>
2025-01-13 04:15:33 +0800

    Full updates

--- app/Http/Controllers/TripRatingController.php
+++ app/Http/Controllers/TripRatingController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\TripRequest;
+use App\Http\Requests\TripRatingRequest;
 use App\Models\TripRating;
 use Illuminate\Http\Request;
 
 class TripRatingController extends Controller
 {
-    public function store(TripRequest $request)
+    public function store(TripRatingRequest $request)
     {
         $validated = $request->validated();
 
